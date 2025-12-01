9a0bb7ea5666e1d662904570302f651c14ea874c
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-12-01 16:50:46 +0300

    add test console

---
+++ Tests/Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KpblcDrzRefactorConsole
+{
+
+    public class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            //EnumDescription enumConverter = new EnumDescription();
+            //enumConverter.StartTest();
+
+            //return;
+
+            //AddKeywordTest addKeywordTest = new AddKeywordTest();
+            //addKeywordTest.StartTest();
+
+            //return;
+            //Console.WriteLine(AppSettings.ConfigPath);
+            //AppSettings.Settings.ApplicationName = "SomeTestApplication";
+            //AppSettings.SaveSettings();
+
+            //DoNotChangeUITest doNotChangeUITest = new DoNotChangeUITest();
+            //doNotChangeUITest.StartTest();
+
+
+            //BlockNormalizeUITest blockNormalizeUiTest = new BlockNormalizeUITest();
+            //blockNormalizeUiTest.StartTest();
+
+            //WindowOwnerUITest windowOwnerUITest = new WindowOwnerUITest();
+            //windowOwnerUITest.StartTest();
+
+            //проверим что там теперь с настройками
+            //SettingsUITest settingsTest = new SettingsUITest();
+            //settingsTest.StartTest();
+
+            //Console.WriteLine("Hello, World!");
+            //Console.ReadKey();
+        }
+    }
+}
