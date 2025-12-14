554805a461346b1f4f9248862de4c5332768e83d
doctorRAZ <doctorRAZ@yandex.ru>
2025-12-14 19:57:47 +0300

    подгон от крыса

--- SpecSPDS.NCad/Services/MulticadProps.cs
+++ SpecSPDS.NCad/Services/MulticadProps.cs
@@ -381,4 +381,20 @@ namespace dRz.SpecSPDS.NCad.Services
         /// </summary>
         Space _space { get; set; }
     }
+	
+	//подгон от крыса
+	   public class NamedProperties
+    {
+        public IEnumerable<McNamedProperty> Get(IEnumerable<McObjectId> objectIds)
+        {
+            return objectIds.Select(o => McObjectManager.GetObject(o) as McUMarker)
+                .Where(o => o != null)
+                .Select((o) =>
+                {
+                    McProperties props = McPropertySource.GetPropertySource(o).ObjectProperties;
+                    // Дальше тут наполнение своего класса необходимыми свойствами
+                    return new McNamedProperty();
+                });
+        }
+    }
 }
