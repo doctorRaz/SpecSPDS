aabe46489948faf9fd948b8c72780370c67583ee
doctorRAZ <doctorRAZ@yandex.ru>
2025-12-15 12:31:59 +0300

    рабочий сбор файлов из папок и подпапок

---
+++ SpecSPDS.Core/Services/FetchingPatchFiles.cs
@@ -0,0 +1,96 @@
+﻿using dRz.SpecSPDS.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dRz.SpecSPDS.NCad.Services
+{
+    public class FetchingPatchFiles
+    {
+        public static List<string> GetFiles(Space space)
+        {
+            //string folderPatch = string.Empty;
+            //string[] filePatchs = new string[] { };
+
+            if (space == Space.Folder || space == Space.SybFolder)
+            {
+                //собрать файлы из каталога
+                return GetFilesOfDir(Browser(), space == Space.SybFolder);
+            }
+            else
+            {           
+                return new List<string>();
+            }
+
+
+        }
+
+        static string Browser()
+        {
+            //https://autolisp.ru/2024/05/23/nanocad-net-select-folder/
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                dlg.Description = "Выберите каталог";
+                dlg.UseDescriptionForTitle = true;
+                // Остальные настройки
+
+                dlg.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(dlg.SelectedPath))
+                {
+                    return dlg.SelectedPath;
+                }
+                return string.Empty;
+            }
+
+        }
+
+
+        static string[] Files()
+        {
+
+            return new string[] { };
+        }
+
+        static List<string> GetFilesOfDir(string sPath, bool WithSubfolders = false, string sSerchPatern = "*.dwg")
+        {
+            try
+            {
+                if (Directory.Exists(sPath))
+                {
+                    return Directory.GetFiles(sPath,
+                                                sSerchPatern,
+                                                (WithSubfolders
+                                                ? SearchOption.AllDirectories
+                                                : SearchOption.TopDirectoryOnly)).ToList<string>();
+                }
+                else
+                {
+
+                    return new List<string>();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                //todo переделать на интерфейс сообщений
+#if NC || AC
+                Cad.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\n" + ex.Message);
+#else
+                MessageBox.Show(ex.Message);
+#endif
+                return new List<string>();
+            }
+
+        }
+
+
+
+
+        //Space _space;
+    }
+}
