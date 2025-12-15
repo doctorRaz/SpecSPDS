ba104f9c1819d81824721e342e3976a294f2cc0e
doctorRAZ <doctorRAZ@yandex.ru>
2025-12-15 20:19:46 +0300

    пример в отдельный класс

--- SpecSPDS.NCad/Services/MulticadProps.cs
+++ SpecSPDS.NCad/Services/MulticadProps.cs
@@ -416,19 +416,5 @@ namespace dRz.SpecSPDS.NCad.Services
         Space _space { get; set; }
     }
 
-    //подгон от крыса
-    public class NamedProperties
-    {
-        public IEnumerable<DefinitionMarkerProps> Get(IEnumerable<McObjectId> objectIds)
-        {
-            return objectIds.Select(o => McObjectManager.GetObject(o) as McUMarker)
-                .Where(o => o != null)
-                .Select((o) =>
-                {
-                    McProperties props = McPropertySource.GetPropertySource(o).ObjectProperties;
-                    // Дальше тут наполнение своего класса необходимыми свойствами
-                    return new DefinitionMarkerProps();
-                });
-        }
-    }
+   
 }
---
+++ SpecSPDS.NCad/TestCommand/NamedProperties.cs
@@ -0,0 +1,28 @@
+﻿using dRz.SpecSPDS.Core.Models;
+using Multicad;
+using Multicad.DatabaseServices;
+using Multicad.Symbols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dRz.SpecSPDS.NCad.TestCommand
+{
+    //подгон от крыса
+    public class NamedProperties
+    {
+        public IEnumerable<DefinitionMarkerProps> Get(IEnumerable<McObjectId> objectIds)
+        {
+            return objectIds.Select(o => McObjectManager.GetObject(o) as McUMarker)
+                .Where(o => o != null)
+                .Select((o) =>
+                {
+                    McProperties props = McPropertySource.GetPropertySource(o).ObjectProperties;
+                    // Дальше тут наполнение своего класса необходимыми свойствами
+                    return new DefinitionMarkerProps();
+                });
+        }
+    }
+}
