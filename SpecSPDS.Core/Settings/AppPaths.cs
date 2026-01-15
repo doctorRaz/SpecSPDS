addad55aa42a4391ddd109aae992f8853c80262d
doctorRAZ <doctorRAZ@yandex.ru>
2026-01-15 23:44:26 +0300

    пути от жопите

---
+++ SpecSPDS.Core/Settings/AppPaths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drz.SpecSPDS.Core.Settings
+{
+    public static class AppPaths
+    {
+        private static string Product => AssemblyInfo.Product;
+        public static string AppRoot =>
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                Product);
+
+        public static string ConfigDir =>
+            Path.Combine(AppRoot, "config");
+
+        public static string LogDir =>
+            Path.Combine(AppRoot, "logs");
+
+        public static string ConfigFile =>
+            Path.Combine(ConfigDir, $"{Product}.config");
+
+        public static void Ensure()
+        {
+            Directory.CreateDirectory(ConfigDir);
+            Directory.CreateDirectory(LogDir);
+        }
+    }
+}
---
+++ SpecSPDS.Core/Settings/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drz.SpecSPDS.Core.Settings
+{
+    public static class AssemblyInfo
+    {
+        public static string Product
+        {
+            get
+            {
+                var asm = Assembly.GetExecutingAssembly();
+                return asm
+                    .GetCustomAttribute<AssemblyProductAttribute>()?.Product
+                       ?? asm.GetName().Name!;
+            }
+        }
+    }
+}
