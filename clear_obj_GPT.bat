2e8037ccdfb151b7f5c2104d9a32dad8c746ac52
doctorRAZ <doctorRAZ@yandex.ru>
2026-02-13 07:39:48 +0300

    батники больше не нужны

--- clear_obj_GPT.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Cleaning obj...
-for /d /r %%d in (obj) do (
-  if exist "%%d" rd /s /q "%%d"
-)
-echo Done. 
--- clear_vs_GPT.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Cleaning .vs...
-for /d /r %%d in (.vs) do (
-  if exist "%%d" rd /s /q "%%d"
-)
-echo Done.
