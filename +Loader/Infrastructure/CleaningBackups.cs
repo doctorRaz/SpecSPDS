d75778c72f897952d1be26e301d220312b6088be
doctorRAZ <doctorRAZ@yandex.ru>
2026-03-16 21:56:31 +0300

    добавил чистилку баков и копий

---
+++ +Loader/Infrastructure/CleaningBackups.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace dRz.Loader.Cad.Infrastructure
+{
+    public class CleaningBackups
+
+    {
+
+        // Регулярное выражение для файлов вида "Имя(число).Расширение"
+        private static Regex digitsInBracketsAtEnd = new Regex(@"\(\d+\)\.\w+$");
+
+        public static void Cleaning(string directoryPath)
+        {
+
+            try
+            {
+                // EnumerateFiles лениво проходит файлы рекурсивно
+                foreach (string? filePath in Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories))
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string extension = Path.GetExtension(filePath);
+
+                    if (extension.Equals(".bak", StringComparison.OrdinalIgnoreCase) || digitsInBracketsAtEnd.IsMatch(fileName))
+                    {
+                        try
+                        {
+                            File.Delete(filePath);
+
+                        }
+                        catch/* (Exception ex)*/
+                        {
+
+                        }
+                    }
+                }
+
+            }
+            catch /*(Exception ex)*/
+            {
+
+            }
+
+        }
+    }
+}
