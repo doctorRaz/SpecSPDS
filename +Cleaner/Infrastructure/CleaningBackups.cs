fafd3f0396c4d4deff51751909babdd56e585317
doctorRAZ <doctorRAZ@yandex.ru>
2026-03-19 20:12:19 +0300

    чистилка сборкой, повысил nf (иначе копирует левые сборки)

---
+++ +Cleaner/Infrastructure/CleaningBackups.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace dRz.Cleaner.Infrastructure
+{
+    /// <summary>
+    /// чистка архивных файлов
+    /// </summary>
+    public class CleaningBackups
+
+    {
+        /// <summary>
+        /// Регулярное выражение для файлов вида "Имя(число).Расширение"
+        /// </summary>
+        //private static Regex digitsInBracketsAtEnd = new Regex(@"\(\d+\)\.\w+$");
+        private static readonly Regex digitsInBracketsAtEnd =
+                                new(@"\(\d+\)\.[^.]+$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        /// <summary>
+        /// чистилка *.bak и *****(111).****
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        public static void Cleaning(string directoryPath)
+        {
+            //digitsInBracketsAtEnd = new Regex(@"\(\d+\)$");
+            //digitsInBracketsAtEnd = new Regex(@"\(\d+\)\.\*");
+            try
+            {
+                // EnumerateFiles лениво проходит файлы рекурсивно
+                foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories))
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    //string fileName = Path.GetFileNameWithoutExtension(filePath);
+                    string extension = Path.GetExtension(filePath);
+
+                    if (extension.Equals(".bak", StringComparison.OrdinalIgnoreCase) || digitsInBracketsAtEnd.IsMatch(fileName))
+                    {
+                        try
+                        {
+                            File.Delete(filePath);
+
+                        }
+                        catch { }
+                    }
+                }
+
+            }
+            catch { }
+
+        }
+    }
+}
