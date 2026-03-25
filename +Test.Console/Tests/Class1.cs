f0d75d67513b1afc3f617699300d7f86b2c7badb
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2026-03-25 08:11:16 +0300

    тест с CAD.Runtime

---
+++ +Test.Console/Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using dRz.CAD.Runtime.Info;
+using System;
+
+namespace dRz.SpecSpds.Test.Tests
+{
+    internal class Class1
+    {
+        public void test()
+        {
+         Console.WriteLine(new InfoCad());
+
+            var os = InfoOs.Current;
+         Console.WriteLine(os);
+
+        }
+    }
+}
