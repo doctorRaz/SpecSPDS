6edd1369660caa45a33aa83bdf3c74196cda31a3
doctorRAZ <doctorRAZ@yandex.ru>
2026-04-10 17:35:44 +0300

    +

---
+++ +Infrastructure.Test/Infrastructure/DocumentService.cs
@@ -0,0 +1,49 @@
+﻿using Abstractions.Services;
+using HostMgd.ApplicationServices;
+using System;
+using System.IO;
+
+namespace Test.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        public bool IsActive
+        {
+            get
+            {
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+                return doc != null;
+            }
+        }
+
+        public string FullPath
+        {
+            get
+            {
+                if (!IsActive)
+                {
+                    throw new NullReferenceException("Нет активного документа");
+                }
+
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+                return doc.Name;
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return Path.GetFileName(FullPath);
+            }
+        }
+
+        public string FileNameNoExtension
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(FullPath);
+            }
+        }
+    }
+}
