942634e95589050ca53f9144f35e0741e040ea6a
doctorRAZ <doctorRAZ@yandex.ru>
2026-04-18 08:51:09 +0300

    разделил сервисы и инфраструктуру, вынес класс в файл

---
+++ src/Infrastructure/CadEnvironmentInfoProvider.cs
@@ -0,0 +1,24 @@
+﻿using drz.Abstractions.Logger;
+
+using drz.EnvironmentInfo;
+using static drz.Src.Infrastructure.AddOnContext;
+
+namespace drz.Src.Infrastructure
+{
+    /// <summary>
+    /// Проброс в фабрику инфы о ОС КАД и аддоне
+    /// </summary>
+    /// <seealso cref="Abstractions.Logger.IEnvironmentInfoProvider" />
+    public class CadEnvironmentInfoProvider : IEnvironmentInfoProvider
+    {
+        /// <summary>
+        /// Gets the summary.
+        /// </summary>
+        /// <returns></returns>
+        public string GetSummary()
+        {
+            //порнография конечно( /InfoDll/
+            return $"{RT.Info.ToString()}\n{InfoDll.ToString()}";
+        }
+    }
+}
