d13e61e0094cdb04d32900376d31a05e58e89b53
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-08-03 20:18:42 +0300

    Directory.Build.props причесал, +Test.Raw\n.Raw.csproj  убрал лишнее

--- +Test.Raw/StartRaw.cs
+++ +Test.Raw/StartRaw.cs
@@ -2,29 +2,29 @@
 using drz.Infrastructure.Infrastructure;
 using System;
 
-namespace drz.SpecSPDS.Test
+namespace dRz.SpecSPDS
 {
     /// <summary>
     /// StartRaw
     /// </summary>
     public class StartRaw
     {
-      
+
 
         [STAThread]
         private static void Main(string[] args)
         {
 
-            IAddOnInfo addOnInfo=new AddOnInfo(typeof(StartRaw).Assembly);
+            IAddOnInfo addOnInfo = new AddOnInfo(typeof(StartRaw).Assembly);
 
             var vendor = addOnInfo.GetMetadata(AssemblyMetadataKeys.Vendor);
             var url = addOnInfo.GetMetadata(AssemblyMetadataKeys.RepositoryUrl);
             var empt = addOnInfo.GetMetadata("");
-            var empt2 = addOnInfo.GetMetadata("","def");
+            var empt2 = addOnInfo.GetMetadata("", "def");
 
             string ret;
 
-            var tryg=addOnInfo.TryGetMetadata("HomePage", out ret);
+            var tryg = addOnInfo.TryGetMetadata("HomePage", out ret);
             Console.WriteLine(addOnInfo.ToLongString());
         }
     }
